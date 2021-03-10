5d3af74a991e585bc75a15543fabb5e2a23dfe81
mapdio <77219481+mapdio@users.noreply.github.com>
2021-03-10 09:48:48 +0800

    Create addons.sh

---
+++ addons.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git clone https://github.com/chongshengB/rt-n56u.git addons
+cp -r addons/user/v2ray user/
+cp -r addons/user/smartdns user/
+cp -r addons/user/trojan user/
+cp -r addons/user/microsocks user/
+
