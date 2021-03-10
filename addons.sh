f785831392cdc3563ba889c917643c90bc4c32f9
mapdio <77219481+mapdio@users.noreply.github.com>
2021-03-10 13:07:30 +0800

    Update addons.sh

--- addons.sh
+++ addons.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 git clone --depth=1 https://github.com/chongshengB/rt-n56u.git /opt/addons
-cp -rf /opt/addons/trunk/user/v2ray /opt/rt-n56u/trunk/user/
-cp -rf /opt/addons/trunk/user/smartdns /opt/rt-n56u/trunk/user/
-cp -rf /opt/addons/trunk/user/trojan /opt/rt-n56u/trunk/user/
-cp -rf /opt/addons/trunk/user/microsocks /opt/rt-n56u/trunk/user/
-cp -rf /opt/addons/trunk/user/kumasocks /opt/rt-n56u/trunk/user/
-cp -f /opt/addons/trunk/user/Makefile /opt/rt-n56u/trunk/user/Makefile
+# cp -rf /opt/addons/trunk/user/v2ray /opt/rt-n56u/trunk/user/
+# cp -rf /opt/addons/trunk/user/smartdns /opt/rt-n56u/trunk/user/
+# cp -rf /opt/addons/trunk/user/trojan /opt/rt-n56u/trunk/user/
+# cp -rf /opt/addons/trunk/user/microsocks /opt/rt-n56u/trunk/user/
+# cp -rf /opt/addons/trunk/user/kumasocks /opt/rt-n56u/trunk/user/
+# cp -f /opt/addons/trunk/user/Makefile /opt/rt-n56u/trunk/user/Makefile
+rsync -avzP /opt/addons/trunk/user /opt/rt-n56u/trunk/user
+
