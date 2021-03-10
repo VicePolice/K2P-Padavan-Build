69d1c5fb7db247efe494940abbbf07e5d9ce6f36
mapdio <77219481+mapdio@users.noreply.github.com>
2021-03-10 11:48:08 +0800

    Update addons.sh

--- addons.sh
+++ addons.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-git clone https://github.com/chongshengB/rt-n56u.git addons
-cp -r addons/trunk/user/v2ray /opt/rt-n56u/trunk/user/
-cp -r addons/trunk/user/smartdns /opt/rt-n56u/trunk/user/
-cp -r addons/trunk/user/trojan /opt/rt-n56u/trunk/user/
-cp -r addons/trunk/user/microsocks /opt/rt-n56u/trunk/user/
-cp -r addons/trunk/user/kumasocks /opt/rt-n56u/trunk/user/
+git clone https://github.com/chongshengB/rt-n56u.git /addons
+cp -r /addons/trunk/user/v2ray /opt/rt-n56u/trunk/user/
+cp -r /addons/trunk/user/smartdns /opt/rt-n56u/trunk/user/
+cp -r /addons/trunk/user/trojan /opt/rt-n56u/trunk/user/
+cp -r /addons/trunk/user/microsocks /opt/rt-n56u/trunk/user/
+cp -r /addons/trunk/user/kumasocks /opt/rt-n56u/trunk/user/
 
