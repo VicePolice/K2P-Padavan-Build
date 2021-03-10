b5066f18e4318c21b0a762765875fd876e56d603
mapdio <77219481+mapdio@users.noreply.github.com>
2021-03-11 00:05:41 +0800

    Update addons.sh

--- addons.sh
+++ addons.sh
@@ -8,5 +8,6 @@ cp -rf /opt/addons/trunk/user/microsocks /opt/rt-n56u/trunk/user/
 cp -rf /opt/addons/trunk/user/kumasocks /opt/rt-n56u/trunk/user/
 cp -rf /opt/addons/trunk/user/chinadns-ng /opt/rt-n56u/trunk/user/
 cp -rf /opt/addons/trunk/user/dns2tcp /opt/rt-n56u/trunk/user/
-cp -rf /opt/addons/trunk/user/SHADOWSOCKS /opt/rt-n56u/trunk/user/
+rm -rf /opt/rt-n56u/trunk/user/shadowsocks
+cp -rf /opt/addons/trunk/user/shadowsocks /opt/rt-n56u/trunk/user/
 cp -f /opt/addons/trunk/user/Makefile /opt/rt-n56u/trunk/user/Makefile
