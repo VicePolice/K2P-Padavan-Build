e1ec551703a1f6994e100be567f45527fceaf996
VicePolice <alexz80@mail.ru>
2021-05-23 12:26:04 +0300

    Update setconfig.sh

--- setconfig.sh
+++ setconfig.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-cp PSG1218-K2-nano.config /opt/rt-n56u/trunk/configs/templates/PSG1218-K2-nano.config
 cp K2P-SSR-5.0.config /opt/rt-n56u/trunk/configs/templates/K2P-SSR-5.0.config
 cd /opt/rt-n56u/trunk/configs/boards
-cp -r PSG1218 PSG1218-K2
+
